2ced1a581b93e071174707978a033ec6a27d61a8
S-kipper <adityasekhartiwariup@gmail.com>
2025-11-07 19:11:56 +0530

    REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
          
+        while(true) {
          System.out.print("$ ");
          Scanner scanner = new Scanner(System.in);
          String input = scanner.nextLine();
+
+
          System.out.println(input + ": command not found");
+
+        }
     }
 }
