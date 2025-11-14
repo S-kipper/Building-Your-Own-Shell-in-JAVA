c946776e1e2d685e4b1315737314fd461c3805ef
S-kipper <adityasekhartiwariup@gmail.com>
2025-11-14 21:32:15 +0530

    Added command not found

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
          System.out.print("$ ");
          Scanner scanner = new Scanner(System.in);
          String input = scanner.nextLine();
-
+         
 
          System.out.println(input + ": command not found");
          scanner.close();
