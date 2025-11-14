a2ccdfdb32a954e47b6706266c914ad1e350f1b3
S-kipper <adityasekhartiwariup@gmail.com>
2025-11-14 22:17:54 +0530

    Adding an exit 0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-         
-        while(true) {
-         System.out.print("$ ");
-         Scanner sc = new Scanner(System.in);
-         String input = sc.nextLine();
-         
-         if(input.equalsIgnoreCase("0"))
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("$ ");
+
+            if (!sc.hasNextLine()) {
+                break;  // no more input from tester → stop
+            }
+
+            String input = sc.nextLine();
+
+            if (input.equalsIgnoreCase("exit 0")) {
                 break;
-         System.out.println(input + ": command not found");
-         sc.close();
+            }
+
+            System.out.println(input + ": command not found");
         }
+
+        sc.close();
     }
 }
