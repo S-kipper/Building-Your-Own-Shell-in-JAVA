5c9acd42b6f0239a8a132b39c02c09490563755d
S-kipper <adityasekhartiwariup@gmail.com>
2025-11-15 09:44:54 +0530

    Added the echo command while checking whether the given input is valid or not

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,8 +23,8 @@ public class Main {
             }
             
 
-        
-            //System.out.println(input + ": command not found");
+            else
+                System.out.println(input + ": command not found");
         }
 
         sc.close();
